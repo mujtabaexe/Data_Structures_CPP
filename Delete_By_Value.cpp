89bee42a1ed70fe1a6a17aa461f12d3d244929fb
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2025-03-10 23:22:44 +0500

    Deletion by value

--- Delete_By_Value.cpp
+++ Delete_By_Value.cpp
@@ -7,39 +7,101 @@ struct Node
     Node *next;
 };
 
-Node *head = 0;
-Node *tail = 0;
+Node *head = NULL;
+Node *tail = NULL;
 
-void insert(int data){
-    if(head== NULL && tail == NULL){
-        Node* newNode = new Node();
+void insert(int data)
+{
+    if (head == NULL && tail == NULL)
+    {
+        Node *newNode = new Node();
         newNode->data = data;
         newNode->next = NULL;
 
         head = newNode;
         tail = newNode;
-    }else{
-        Node* newNode = new Node();
+    }
+    else
+    {
+        Node *newNode = new Node();
         newNode->data = data;
         newNode->next = head;
 
         head = newNode;
     }
 }
-void print(){
-    Node* temp = head;
-    while(temp!=NULL){
-        cout << temp->data<< " ";
+
+void valDelete(int data)
+{
+    if (head->data == data && tail->data == data)
+    {
+        Node *temp = head;
+        delete (temp);
+        head = NULL;
+        tail = NULL;
+
+        return;
+    }
+
+    else if (head->data == data)
+    {
+        Node *temp = head;
+        head = head->next;
+        delete (temp);
+        return;
+    }
+
+    else if (tail->data == data)
+    {
+        Node *temp = head;
+
+        while (temp->next != tail)
+        {
+            temp = temp->next;
+        }
+
+        tail = temp;
+        tail->next = NULL;
+
+        delete(temp->next);
+
+        return;
+    }
+
+    Node *temp = head;
+    Node *tempForward = temp->next;
+    while (tempForward != NULL)
+    {
+        if (tempForward->data == data)
+        {
+            temp->next = tempForward->next;
+
+            return;
+        }
         temp = temp->next;
+        tempForward = tempForward->next;
     }
 }
+void print()
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
 
-int main(){
+int main()
+{
     insert(10);
     insert(20);
     insert(30);
     insert(40);
     insert(50);
 
+    print();
+    valDelete(10);
     print();
 }
